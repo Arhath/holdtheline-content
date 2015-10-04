71437ca95d6d8affc063a57362ccf9cedb3bf131
Joshua <arhath@web.de>
2015-10-04 18:36:17 +0200

    changes to match glyphs
    glyphs

--- panorama/scripts/clicks.js
+++ panorama/scripts/clicks.js
@@ -20,6 +20,25 @@ function OnRightButtonPressed(event)
 	return false;
 }
 
+function OnLeftButtonPressed(event)
+{
+	//$.Msg("OnRightButtonPressed")
+
+	var iPlayerID = Players.GetLocalPlayer();
+	var mainSelected = Players.GetLocalPlayerPortraitUnit(); 
+	var mPos = GameUI.GetCursorPosition();
+	var GamePos = Game.ScreenXYToWorld(mPos[0], mPos[1]);
+	var mouseEntities = GameUI.FindScreenEntities( mPos );
+	mouseEntities = mouseEntities.filter( function(e) { return e.entityIndex != mainSelected; } )
+	
+	// Unit rightclick
+	
+
+	GameEvents.SendCustomGameEventToServer( "unit_left_click", { pID: iPlayerID, mainSelected: mainSelected, mouseX: GamePos[0], mouseY: GamePos[1] , name: event})
+
+	return false;
+}
+
 function IsBuilder(name) {
 	return (name == "human_peasant" || name == "nightelf_wisp" || name == "orc_peon" || name == "undead_acolyte")
 }
@@ -40,7 +59,8 @@ GameUI.SetMouseCallback( function( eventName, arg ) {
 		if ( arg === 0 )
 		{
 			//OnLeftButtonPressed();
-			return CONTINUE_PROCESSING_EVENT;
+			//return CONTINUE_PROCESSING_EVENT;
+			return OnLeftButtonPressed(eventName);
 		}
 
 		// Right-click
